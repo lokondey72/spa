9e3ba45ebffd56504d113e5c1418ef5f2dddf4f9
Lokon Dey <lokondey72@gmail.com>
2025-04-17 12:25:47 +0600

    17-04 all problems commit

--- src/app/choice/page.js
+++ src/app/choice/page.js
@@ -81,7 +81,7 @@ const products = [
 // app/choice/page.js
 export const dynamic = "force-dynamic";
 
-export default function HomePage() {
+export default function choicePage() {
   const [selectedProduct, setSelectedProduct] = useState("");
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
